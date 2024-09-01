5279e55641065b86ad5432ee85d07256bdb616f1
plunktone <arseorel@gmail.com>
2024-09-01 10:46:26 +0300

    spawn gameobject with delay

--- Assets/Scripts/SpawnPrefabs.cs
+++ Assets/Scripts/SpawnPrefabs.cs
@@ -7,15 +7,24 @@ public class SpawnPrefabs : MonoBehaviour
     public GameObject spawnPrefab;
     public Transform spawnTransform;
     public int spawnCount;
+    private float spawnDelay = 1f;
 
-    public void Spawn()
+
+    private void Start() {
+        
+        StartCoroutine(Spawn());
+    }
+    public IEnumerator Spawn()
     {
         for (int i = 0; i < spawnCount; i++) 
         {
-            Instantiate(spawnPrefab, spawnTransform);
+            
+            GameObject spawnInst  = Instantiate(spawnPrefab, spawnTransform);
+            Vector2 randomDirection = Random.insideUnitCircle * 5f;
+            spawnInst.GetComponent<Rigidbody2D>().velocity = randomDirection;
 
         }
 
-
+        yield return new WaitForSeconds(spawnDelay);
     }
 }
